b9020f0b567596cdc165abfe1ffb19292578f57c
3tas <sdhruvemail@gmail.com>
2022-09-18 09:04:39 -0400

    updates

---
+++ ReactPain/flask-server/server.py
@@ -0,0 +1,23 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+
+
+@app.route("/members")
+def members():
+    return{"members":["member1", "member2", "member3"]}
+
+@app.route("/googleImage")
+def googleImage():
+    images=[]
+    return images
+
+@app.route(eyeTracker)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='localhost', port=8000)
+    
